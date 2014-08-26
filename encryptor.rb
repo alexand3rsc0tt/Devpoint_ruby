0e3de7a9733739c44973360b65e13ee417deab89
alexand3rsc0tt <alex.burton1993@gmail.com>
2014-08-26 15:52:02 -0600

    add encryptor.rb

---
+++ encryptor.rb
@@ -0,0 +1,44 @@
+class Encryptor
+  def encrypt_letter(letter)
+	lowercase_letter = letter.downcase
+	cipher[lowercase_letter]
+  end
+  		def encrypt(string)
+  	  # 1. Cut the input string into letters
+  	  letters = string.split("")
+
+  	  # 2. Encrypt those letters one at a time, gathering the results
+  	  results = []
+  	  letters.each do |letter|
+  	  	encrypted_letter = encrypt_letter(letter)
+  	  	results.push(encrypted_letter)
+  	 	end
+      # 3. Join the results back together in one string
+      results.join
+end
+
+
+  def decrypt_letter(letter)
+		lowercase_letter = letter.downcase
+		cipher[lowercase_letter]
+  end
+	  def decrypt(string)
+ 				letters = string.split("")
+
+ 				results = []
+ 				letters.each do |letter|
+ 					decrypted_letter = decrypt_letter(letter)
+ 					results.push(decrypted_letter)
+ 		 end
+ 				results.join
+end
+	def cipher
+    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+     'y' => 'l', 'z' => 'm'} 	
+	end
+end
